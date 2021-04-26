e94af10374a01897530531370912fbfc8350a569
JH_Joo <wjdgnsx94@naver.com>
2021-04-26 16:50:28 +0900

    feat: add CustomButton

---
+++ exec/App_Employee/CustomButton.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { StyleSheet, Text, TouchableOpacity } from "react-native";
+
+const CustomButton = ({ title, onPress }) => {
+  return (
+    <TouchableOpacity
+      style={styles.button}
+      activeOpacity={0.8}
+      onPress={onPress}
+    >
+      <Text style={styles.text}>{title}</Text>
+    </TouchableOpacity>
+  );
+};
+const styles = StyleSheet.create({
+  button: {
+    alignItems: "center",
+    justifyContent: "center",
+    height: 45,
+    backgroundColor: "#0478C5",
+    borderColor: 0,
+    borderWidth: 1,
+    borderRadius: 12,
+    marginTop: '5%',
+    marginRight: '10%',
+    marginLeft: '10%',
+    alignSelf: "stretch",
+    textAlign: "center",
+  },
+  text: {
+    fontSize: 15,
+    color: 'white',
+    fontWeight: "bold",
+  },
+});
+
+export default CustomButton;
