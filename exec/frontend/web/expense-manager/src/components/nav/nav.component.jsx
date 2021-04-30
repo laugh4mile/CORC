b014a83957df12849b645429249e1962dbef735b
Hyejin Lee <hichjay@gmail.com>
2021-05-01 03:51:20 +0900

    feat: nav component 생성

--- exec/frontend/web/expense-manager/src/components/nav/nav.component.jsx
+++ exec/frontend/web/expense-manager/src/components/nav/nav.component.jsx
@@ -2,18 +2,16 @@ import React from "react";
 
 import { Link } from "react-router-dom";
 
-import {ReactComponent as DashboardIcon } from "../../assets/dashboard.svg"
+import { ReactComponent as DashboardIcon } from "../../assets/dashboard.svg";
 
 const Nav = () => (
-    <div className="nav">
-        <Link to="/dashboard" className="nav-item">
-            <DashboardIcon className="icon" />
-        </Link>
-        <Link to="/user/register">
-
-        </Link>
-
-    </div>
+  <div className="nav">
+    <Link to="/" className="nav-item">
+      <DashboardIcon className="icon" />
+      <span>대쉬보드</span>
+    </Link>
+    <Link to="/user/register"></Link>
+  </div>
 );
 
 export default Nav;
