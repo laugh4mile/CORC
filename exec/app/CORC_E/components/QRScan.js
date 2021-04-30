5d8be6d18f74b44d6b9fb953fdecb29b792dab16
Hyunsoo <laugh4mile@naver.com>
2021-04-30 11:31:40 +0900

    feat: S04P31A301-135 add QR Code Scan Component

--- exec/app/CORC_E/components/QRScan.js
+++ exec/app/CORC_E/components/QRScan.js
@@ -1,12 +1,40 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
+import { BarCodeScanner } from 'expo-barcode-scanner';
 
 export default function Home() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    alert(`${data}`);
+  };
+
+  if (hasPermission === null) {
+    return <Text>Requesting for camera permission</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
   return (
     <View style={styles.container}>
-      <View style={styles.contents}>
-        <Text>여기가 QR이다</Text>
-      </View>
+      <BarCodeScanner
+        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+        // style={StyleSheet.absoluteFillObject}
+        style={styles.barCodeView}
+      />
+      {scanned && (
+        <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />
+      )}
     </View>
   );
 }
@@ -14,10 +42,15 @@ export default function Home() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   contents: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+  },
+  barCodeView: {
+    width: '100%',
+    height: '100%',
+    // marginBottom: 20,
   },
 });
--- exec/app/CORC_E/components/Settings.js
+++ exec/app/CORC_E/components/Settings.js
@@ -3,21 +3,41 @@ import { View, Text, StyleSheet } from 'react-native';
 
 export default function Home() {
   return (
+    // <View style={styles.container}>
+    //   <View style={styles.contents}>
+    //     <Text>여기가 환경설정이다</Text>
+    //   </View>
+    // </View>
     <View style={styles.container}>
-      <View style={styles.contents}>
-        <Text>여기가 환경설정이다</Text>
+      <View style={styles.center}>
+        <View style={styles.behind}></View>
       </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  // container: {
+  //   flex: 1,
+  // },
+  // contents: {
+  //   flex: 1,
+  //   justifyContent: 'center',
+  //   alignItems: 'center',
+  // },
   container: {
     flex: 1,
-  },
-  contents: {
-    flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
+    height: 100,
+    justifyContent: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  center: {
+    width: 200,
+    height: 200,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'rgba(255, 255, 255, 0.1)',
+    borderRadius: 15,
   },
 });
