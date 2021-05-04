b86eeaa84be28ea550b39e342d5b699299b97eb2
Hyunsoo <laugh4mile@naver.com>
2021-05-05 05:03:27 +0900

    feat: S04P31A301-167 autoLogin and Loading

--- exec/app/CORC_E/App.js
+++ exec/app/CORC_E/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, View, Platform } from 'react-native';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
@@ -8,43 +8,51 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import authReducer from './store/reducers/auth';
 import Navigation from './navigations/Navigation';
+import Loading from './screens/Loading';
+import BottomTabNavigation from './navigations/BottomTabNavigation';
 
 const rootReducer = combineReducers({
   auth: authReducer,
 });
-
 const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
 
 export default class extends React.Component {
-  constructor() {
-    super();
-    console.log(this.getToken());
+  async getToken(user) {
+    try {
+      let userData = await AsyncStorage.getItem('userData');
+      let data = JSON.parse(userData);
+      console.log('good : ', data);
+      this.setState({
+        isLoading: false,
+        data: data,
+      });
+    } catch (error) {
+      console.log('Something went wrong', error);
+    }
   }
-  // async getToken(user) {
-  //   try {
-  //     let userData = await AsyncStorage.getItem('userData');
-  //     let data = JSON.parse(userData);
-  //     console.log('good : ', data);
-  //   } catch (error) {
-  //     console.log('Something went wrong', error);
-  //   }
-  // }
-  getToken = async () => {
-    const userToken = await AsyncStorage.getItem('userData');
+
+  state = {
+    isLoading: true,
   };
-  state = {};
   compo;
   componentDidMount() {
-    // this.getToken();
+    this.getToken();
   }
   render() {
-    return (
+    const { isLoading, data } = this.state;
+    console.log('isLoading : ', isLoading);
+    console.log('data : ', data);
+    return isLoading ? (
+      <Loading />
+    ) : !data ? (
       <Provider store={store}>
         <View style={styles.container}>
           <Navigation />
           <StatusBar style="auto" />
         </View>
       </Provider>
+    ) : (
+      <BottomTabNavigation />
     );
   }
 }
