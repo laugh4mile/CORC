8b4134efa24c754f352ba58d22e2b6676d2603f3
Hyunsoo <laugh4mile@naver.com>
2021-05-06 02:24:11 +0900

    refector: delete AsyncStorage at Home

--- exec/app/CORC_E/screens/Home.js
+++ exec/app/CORC_E/screens/Home.js
@@ -1,18 +1,10 @@
 import React from 'react';
 import { View, Text, StyleSheet, Platform } from 'react-native';
 import Constants from 'expo-constants';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-
-async function getUser() {
-  const userData = await AsyncStorage.getItem('userData');
-  return userData;
-}
 
 export default function Home() {
-  const user = getUser();
-  // console.log(user);
-  // console.log('userData : ', AsyncStorage.getItem('userData'));
   return (
+    // console.log()
     <View style={styles.container}>
       <View style={styles.contents}>
         <Text>여기가 홈이다 </Text>
