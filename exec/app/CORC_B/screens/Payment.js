329ec7b360fe013eeb76499c957c9e282e3e631b
JH_Joo <wjdgnsx94@naver.com>
2021-05-07 15:45:45 +0900

    feat: S04P31A301-175 주문서 항목 추가 및 QR 생성

--- exec/app/CORC_B/screens/Payment.js
+++ exec/app/CORC_B/screens/Payment.js
@@ -1,25 +1,58 @@
-import React, { useState } from "react";
-import { StyleSheet, Text, View, ScrollView, Alert } from "react-native";
+import React, { useState, useEffect } from "react";
+import {
+  Alert,
+  Dimensions,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
 import Modal from "react-native-modal";
-import BarcodeScan from "./Payment/BarcodeScan";
+import QRCode from "react-native-qrcode-svg";
+import { useSelector } from "react-redux";
 
+import Colors from "../constants/Colors";
 import Button from "../components/Button";
 import Card from "../components/Card";
 import Input from "../components/Input";
-import Colors from "../constants/Colors";
+import BarcodeScan from "./Payment/BarcodeScan";
+
+const { width } = Dimensions.get("window");
+const imgSize = width * 0.06;
+const qrSize = width * 0.55;
 
 const Payment = (props) => {
+  const userId = useSelector((state) => state.auth.userId);
   const [scanOpened, setScanOpened] = useState(false);
   const [isModalVisible, setModalVisible] = useState(false);
+  const [isVacateModalVisible, setVacateModalVisible] = useState(false);
   const [items, setItems] = useState([]);
 
   const [productName, setProductName] = useState("");
   const [price, setPrice] = useState(0);
   const [quantity, setQuantity] = useState(0);
+  const [total, setTotal] = useState(0);
+
+  const [qrVisible, setQRVisible] = useState(false);
+
+  useEffect(() => {
+    getSum();
+  }, [items]);
+
+  const getSum = () => {
+    let sum = 0;
+    for (let index = 0; index < items.length; index++) {
+      sum += items[index].amount * items[index].price;
+    }
+    setTotal(sum);
+  };
 
   const toggleModal = () => {
     setModalVisible(!isModalVisible);
   };
+
   const addItem = () => {
     var spExp = /[^가-힣ㄱ-ㅎㅏ-ㅣa-zA-Z0-9]/gi; // special character
     var numExp = /^\d+$/; // number expression
@@ -34,69 +67,424 @@ const Payment = (props) => {
       return Alert.alert(null, "수량을 확인해주세요.");
     }
 
-    let total = quantity * price;
     setItems([
       ...items,
-      { name: productName, price: price, quantity: quantity, total: total },
+      {
+        productName: productName,
+        price: price,
+        amount: quantity,
+      },
     ]);
     toggleModal();
   };
+
+  const addScannedItem = (data) => {
+      let jsonData = JSON.parse(data);
+      console.log(jsonData.productName, jsonData.price, jsonData.amount)
+
+        let productName = jsonData.productName
+        let price = jsonData.price.replace(/[^0-9.]/g, "").replace(/(\..*)\./g, "$1");
+      setItems([
+        ...items,
+        {
+          productName: productName,
+          price: price,
+          amount: "1",
+        },
+      ]);
+
+      setScanOpened(false)
+  }
+
   const cancelAddItem = () => {
     setProductName("");
     setPrice(0);
     setQuantity(0);
     toggleModal();
   };
+
   const reset = () => {
     setItems([]);
+    setVacateModalVisible(false);
+  };
+
+  const removeItem = (index) => {
+    if (index > -1) {
+      var copied = items
+        .slice(0, index)
+        .concat(items.slice(index + 1, items.length));
+    }
+    setItems(copied);
+  };
+
+  const amountHander = (index, data) => {
+    if (index > -1) {
+      data = data.replace(/[^0-9.]/g, "").replace(/(\..*)\./g, "$1");
+      var copied = items;
+      copied[index].amount = data;
+    }
+    setItems(copied);
+    getSum();
+  };
+
+  const createPayment = () => {
+    if (!items || items.length <= 0) {
+      return Alert.alert(null, "주문할 내역이 없습니다. 확인해주세요.");
+    }
+    var spExp = /[^가-힣ㄱ-ㅎㅏ-ㅣa-zA-Z0-9]/gi; // special character
+    var numExp = /^\d+$/; // number expression
+    // const numExp = /^[0-9]*$/
+    for (let index = 0; index < items.length; index++) {
+      let e = items[index];
+      if (spExp.test(e.productName) || e.productName.trim().length <= 0) {
+        return Alert.alert(
+          null,
+          `${index + 1}번째 품목의 이름을 다시 한 번 확인해주세요.`
+        );
+      }
+      if (!numExp.test(e.price.toString()) || e.price <= 0) {
+        return Alert.alert(
+          null,
+          `${index + 1}번째 품목의 가격을 다시 한 번 확인해주세요.`
+        );
+      }
+      if (!numExp.test(e.amount.toString()) || e.amount <= 0) {
+        return Alert.alert(
+          null,
+          `${index + 1}번째 품목의 수량을 다시 한 번 확인해주세요.`
+        );
+      }
+    }
+    setQRVisible(true);
+  };
+
+  const GenerateQR = (props) => {
+    let size = props.size ? props.size : 100;
+    let data = {
+      storeid: userId,
+      orderList: items,
+      total: total,
+    };
+    console.log(JSON.stringify(data));
+    return <QRCode value={JSON.stringify(data)} size={size} />;
   };
 
   return (
     <View style={styles.container}>
-      <View style={{ flexDirection: "row", justifyContent: "space-evenly" }}>
-        <Button title="아이템 추가" onPress={() => toggleModal()} />
-        {scanOpened ? (
-          <Button title="스캐너 끄기" onPress={() => setScanOpened(false)} />
-        ) : (
-          <Button title="스캐너 켜기" onPress={() => setScanOpened(true)} />
-        )}
-        <Button title="비우기" onPress={() => reset()} color="#bbbbbb" />
-      </View>
-      <ScrollView style={styles.list}>
-        {items.length > 0 ? (
-          items.map((item, index) => {
-            // console.log(item);
-            return (
-              <Text key={index}>
-                {item.name} : {item.price} : {item.quantity} 총합: {item.total}
-              </Text>
-            );
-          })
-        ) : (
-          <Text>아이템을 추가해주세요.</Text>
-        )}
-      </ScrollView>
-      {scanOpened && (
-        <View style={styles.scanbox}>
-          <BarcodeScan />
+      <View
+        style={{
+          flexDirection: "row",
+          height: "10%",
+          alignItems: "flex-end",
+          paddingHorizontal: "10%",
+          marginBottom: 10,
+        }}
+      >
+        <View style={{ flex: 1 }}>
+          <Text style={{ fontWeight: "bold", fontSize: imgSize }}>주문서</Text>
         </View>
-      )}
+
+        <FontAwesome.Button
+          name="plus-square"
+          onPress={() => toggleModal()}
+          backgroundColor="white"
+          color="#a5a5a8"
+          size={imgSize}
+          underlayColor="white"
+          iconStyle={{ marginRight: 0 }}
+          activeOpacity={0.6}
+        />
+        <FontAwesome.Button
+          name="qrcode"
+          onPress={() => setScanOpened(!scanOpened)}
+          backgroundColor="white"
+          color={scanOpened ? "#7986FF" : "#a5a5a8"}
+          size={imgSize}
+          underlayColor="white"
+          iconStyle={{ marginRight: 0 }}
+        />
+        <Modal isVisible={scanOpened}>
+          <BarcodeScan
+            onCancel={() => setScanOpened(false)}
+            onScanned={(data) => addScannedItem(data)}
+          />
+        </Modal>
+      </View>
+      <Card style={styles.list}>
+        <View
+          style={{
+            flexDirection: "row",
+            marginVertical: 5,
+            alignItems:'center',
+          }}
+        >
+          <View style={{ flex: 3.5, alignItems: "center" }}>
+            <Text style={{ fontWeight: "bold", fontSize: 17 }}>품명</Text>
+          </View>
+          <View style={{ flex: 2.5, alignItems: "center" }}>
+            <Text style={{ fontWeight: "bold", fontSize: 17 }}>가격</Text>
+          </View>
+          <View style={{ flex: 1, alignItems: "center" }}>
+            <Text style={{ fontWeight: "bold", fontSize: 17 }}>수량</Text>
+          </View>
+          <View style={{}}>
+            <FontAwesome.Button
+              name="trash"
+              onPress={() => setVacateModalVisible(true)}
+              backgroundColor="white"
+              color="#a5a5a8"
+              underlayColor="white"
+              activeOpacity={0.6}
+              iconStyle={{ }}
+            />
+            <Modal isVisible={isVacateModalVisible}>
+              <Card style={{ ...styles.modalBox, height: "25%" }}>
+                <View
+                  style={{
+                    flex: 4,
+                    alignItems: "center",
+                    justifyContent: "center",
+                  }}
+                >
+                  <Text style={{ fontWeight: "bold", fontSize: 20 }}>
+                    정말로 비우시겠습니까?
+                  </Text>
+                </View>
+                <View
+                  style={{
+                    flexDirection: "row",
+                    justifyContent: "center",
+                    marginTop: 10,
+                  }}
+                >
+                  <View style={{ flex: 1 }}>
+                    <Button title="확인" onPress={() => reset()} />
+                  </View>
+                  <View style={{ flex: 1 }}>
+                    <Button
+                      title="취소"
+                      onPress={() => setVacateModalVisible(false)}
+                      backgroundColor={Colors.cancel.backgroundColor}
+                      fontColor={Colors.cancel.fontColor}
+                    />
+                  </View>
+                </View>
+              </Card>
+            </Modal>
+          </View>
+        </View>
+        <ScrollView style={styles.list}>
+          {items.length > 0 ? (
+            items.map((item, index) => (
+              <View
+                key={index}
+                style={{
+                  flexDirection: "row",
+                  alignItems: "center",
+                }}
+              >
+                <View style={{ flex: 3.5, alignItems: "center" }}>
+                  <Text style={{ fontSize: 15 }}>{item.productName}</Text>
+                </View>
+                <View style={{ flex: 2.5, alignItems:'center'}}>
+                  <Text style={{ textAlign: "right" }}>{item.price} 원</Text>
+                </View>
+                <View style={{ flex: 1, alignItems: "center" }}>
+                  <TextInput
+                    style={{ textAlign: "center" }}
+                    onChangeText={(amount) => amountHander(index, amount)}
+                    keyboardType="numeric"
+                    defaultValue={item.amount}
+                  />
+                </View>
+                <View>
+                  <FontAwesome.Button
+                    name="minus-square-o"
+                    backgroundColor="white"
+                    color="#a5a5a8"
+                    underlayColor="white"
+                    iconStyle={{ marginVertical: -8 }}
+                    activeOpacity={0.6}
+                    onPress={() => removeItem(index)}
+                  />
+                </View>
+              </View>
+            ))
+          ) : (
+            <View style={{ flex: 1, marginTop: 10, alignItems: "center" }}>
+              <Text style={{ color: "gray" }}>아이템을 추가해주세요.</Text>
+            </View>
+          )}
+        </ScrollView>
+      </Card>
+      {/* <View style={styles.scanbox}>
+        {scanOpened && <BarcodeScan onScanned={(data) => scanItem(data)} />}
+      </View> */}
+      <View
+        style={{
+          flex: 1,
+          paddingHorizontal: "10%",
+          justifyContent: "space-evenly",
+          marginBottom: 20,
+        }}
+      >
+        <View>
+          <Text style={{ fontWeight: "bold", fontSize: imgSize }}>
+            결제 예정 금액
+          </Text>
+        </View>
+        <View style={{ alignItems: "flex-end" }}>
+          <Text style={{ fontWeight: "bold", fontSize: imgSize }}>
+            <Text style={{ fontSize: imgSize * 1.2 }}>{total}</Text> 원
+          </Text>
+        </View>
+        <Button
+          title="결제하기"
+          onPress={() => {
+            createPayment();
+          }}
+        />
+        <Modal isVisible={qrVisible} backdropOpacity={0.1}>
+          <Card
+            style={{
+              flex: 1,
+              backgroundColor: "white",
+              borderRadius: 12,
+              borderWidth: 0,
+              marginHorizontal: "0%",
+              paddingHorizontal: "10%",
+              paddingVertical: "5%",
+            }}
+          >
+            <View
+              style={{
+                flex: 1.7,
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <GenerateQR size={qrSize} />
+            </View>
+
+            <View style={{ flex: 1.5 }}>
+              <Text style={{ fontWeight: "bold", fontSize: imgSize }}>
+                주문서
+              </Text>
+
+              <Card
+                style={{
+                  ...styles.list,
+                  marginHorizontal: "0%",
+                  paddingRight: 10,
+                }}
+              >
+                <View
+                  style={{
+                    flexDirection: "row",
+                    marginVertical: 7,
+                  }}
+                >
+                  <View style={{ flex: 3, alignItems: "center" }}>
+                    <Text style={{ fontWeight: "bold", fontSize: 15 }}>
+                      품명
+                    </Text>
+                  </View>
+                  <View style={{ flex: 2, alignItems: "center" }}>
+                    <Text style={{ fontWeight: "bold", fontSize: 15 }}>
+                      가격
+                    </Text>
+                  </View>
+                  <View style={{ flex: 1, alignItems: "center" }}>
+                    <Text style={{ fontWeight: "bold", fontSize: 15 }}>
+                      수량
+                    </Text>
+                  </View>
+                </View>
+                <ScrollView style={styles.list}>
+                  {items.map((item, index) => (
+                    <View
+                      key={index}
+                      style={{
+                        flexDirection: "row",
+                        marginVertical: 3,
+                      }}
+                    >
+                      <View style={{ flex: 3, alignItems: "center" }}>
+                        <Text>{item.productName}</Text>
+                      </View>
+                      <View style={{ flex: 2, alignItems: "center" }}>
+                        <Text>{item.price} 원</Text>
+                      </View>
+                      <View style={{ flex: 1, alignItems: "center" }}>
+                        <Text>{item.amount}</Text>
+                      </View>
+                    </View>
+                  ))}
+                </ScrollView>
+              </Card>
+            </View>
+            <View
+              style={{
+                flex: 1,
+                justifyContent: "space-evenly",
+                paddingTop: 7,
+              }}
+            >
+              <View>
+                <Text style={{ fontWeight: "bold", fontSize: imgSize }}>
+                  결제할 금액
+                </Text>
+              </View>
+              <View style={{ alignItems: "flex-end", marginBottom: 10 }}>
+                <Text style={{ fontWeight: "bold", fontSize: imgSize }}>
+                  <Text style={{ fontSize: imgSize * 1.2 }}>{total}</Text> 원
+                </Text>
+              </View>
+              <Button
+                title="결제 대기 중"
+                onPress={() => {
+                  Alert.alert(
+                    null,
+                    "서비스 준비중입니다.\n잠시만 기다려 주세요!"
+                  );
+                }}
+                backgroundColor="#b4b4b4"
+                fontColor="white"
+              />
+
+              <View style={styles.footerItems}>
+                <Text
+                  onPress={() => {
+                    setQRVisible(false);
+                  }}
+                  style={styles.textlink}
+                >
+                  취소
+                </Text>
+              </View>
+            </View>
+          </Card>
+        </Modal>
+      </View>
 
       <Modal isVisible={isModalVisible}>
         <Card style={styles.modalBox}>
           <Input
             placeholder="이름"
             onChangeText={(name) => setProductName(name)}
+            value={productName}
           />
           <Input
             placeholder="가격"
             onChangeText={(price) => setPrice(price)}
             keyboardType="numeric"
+            value={price}
           />
           <Input
             placeholder="수량"
             onChangeText={(quantity) => setQuantity(quantity)}
             keyboardType="numeric"
+            value={quantity}
           />
           <View
             style={{
@@ -129,17 +517,25 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "white",
-    // justifyContent: 'space-evenly',
-    // alignItems: 'center'
   },
   list: {
-    // flex: 2,
-    height: "70%",
+    marginBottom: 10,
+    flex: 2,
   },
   scanbox: {
-    height: "20%",
+    flex: 1,
+    paddingVertical: 10,
   },
   modalBox: {
     padding: 10,
+  },
+  footerItems: {
+    alignItems: "center",
+    marginTop: 5
+  },
+  textlink: {
+    textDecorationLine: "underline",
+    textDecorationColor: "#696A6E",
+    color: "#696A6E",
   },
 });
--- exec/app/CORC_B/screens/Payment/BarcodeScan.js
+++ exec/app/CORC_B/screens/Payment/BarcodeScan.js
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import { Image, Text, View, StyleSheet, Button } from 'react-native';
-import { BarCodeScanner } from 'expo-barcode-scanner';
-import { Dimensions } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { Image, Text, View, StyleSheet, Button } from "react-native";
+import { BarCodeScanner } from "expo-barcode-scanner";
+import { Dimensions } from "react-native";
 
-const { width } = Dimensions.get('window');
+const { width } = Dimensions.get("window");
 const qrSize = width * 0.7;
 
 export default function BarcodeScan(props) {
@@ -13,13 +13,14 @@ export default function BarcodeScan(props) {
   useEffect(() => {
     (async () => {
       const { status } = await BarCodeScanner.requestPermissionsAsync();
-      setHasPermission(status === 'granted');
+      setHasPermission(status === "granted");
     })();
   }, []);
 
   const handleBarCodeScanned = ({ type, data }) => {
     setScanned(true);
-    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+    props.onScanned(data);
+    // alert(`Bar code with type ${type} and data ${data} has been scanned!`);
   };
 
   if (hasPermission === null) {
@@ -33,12 +34,15 @@ export default function BarcodeScan(props) {
     <View
       style={{
         flex: 1,
-        flexDirection: 'column',
-        // justifyContent: 'flex-end',
-      }}>
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "black",
+      }}
+    >
       <BarCodeScanner
         onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
-        style={[styles.container, {minWidth: '100%'}]}>
+        style={[styles.container]}
+      >
         <Text style={styles.description}>Scan your QR code</Text>
         {/* <Image
           style={styles.qr}
@@ -46,12 +50,19 @@ export default function BarcodeScan(props) {
             uri: 'https://facebook.github.io/react-native/img/tiny_logo.png',
           }}
         /> */}
-        <Text onPress={() => alert("Navigate back from here")} style={styles.cancel}>
+        <Text
+          onPress={() => {
+            props.onCancel();
+          }}
+          style={styles.cancel}
+        >
           Cancel
         </Text>
       </BarCodeScanner>
       {scanned && (
-        <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />
+        <Text onPress={() => setScanned(false)} style={styles.cancel}>
+          Tap to Scan Again
+        </Text>
       )}
     </View>
   );
@@ -60,28 +71,32 @@ export default function BarcodeScan(props) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#ecf0f1',
-    padding: 8,
+    justifyContent: "space-evenly",
+    alignItems: "center",
+    backgroundColor: "gray",
+    width: "150%",
   },
   qr: {
-    marginTop: '20%',
-    marginBottom: '20%',
+    marginTop: "20%",
+    marginBottom: "20%",
     width: qrSize,
     height: qrSize,
   },
   description: {
-    fontSize: width * 0.09,
-    marginTop: '10%',
-    textAlign: 'center',
-    width: '70%',
-    color: 'white',
+    fontSize: width * 0.05,
+    marginBottom: "80%",
+    padding: 3,
+    textAlign: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.2)",
+    color: "white",
+    borderRadius: 12,
   },
   cancel: {
     fontSize: width * 0.05,
-    textAlign: 'center',
-    width: '70%',
-    color: 'white',
+    padding: 3,
+    textAlign: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.2)",
+    color: "white",
+    borderRadius: 12,
   },
 });
