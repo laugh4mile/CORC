e07643c83926324efdf8bb4ce84ad75010a91e51
Hyejin Lee <hichjay@gmail.com>
2021-05-10 03:33:07 +0900

    feat: 사용자 상세 결제내역 페이지 생성 및 연결

---
+++ exec/frontend/web/expense-manager/src/pages/User/UserDetailPage.js
@@ -0,0 +1,18 @@
+import { useParams, useLocation } from "react-router-dom";
+
+const PaymentDetailPage = () => {
+  const params = useParams();
+  const location = useLocation();
+  const { userName } = location.state;
+
+  console.log("params", params);
+  console.log("state", location.state);
+
+  return (
+    <section className="page">
+      <span className="title">{`${userName} (${params.employeeNum})`}</span>
+    </section>
+  );
+};
+
+export default PaymentDetailPage;
