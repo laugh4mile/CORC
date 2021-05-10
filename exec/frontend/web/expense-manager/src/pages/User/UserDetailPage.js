5debfb73ee0e1798396a44864be9db96ed352b6c
Hyejin Lee <hichjay@gmail.com>
2021-05-10 18:24:33 +0900

    feat: 사용자 정보, 사용자 로그 페이지 생성 (탭으로 연결)
    각 페이지별 상세 내용은 작업중

--- exec/frontend/web/expense-manager/src/pages/User/UserDetailPage.js
+++ exec/frontend/web/expense-manager/src/pages/User/UserDetailPage.js
@@ -1,16 +1,63 @@
-import { useParams, useLocation } from "react-router-dom";
+import { useState } from "react";
+import { useParams, useLocation, useHistory } from "react-router-dom";
+
+import UserInfo from "../../components/User/UserInfo";
+import UserLog from "../../components/User/UserLog";
+import Card from "../../components/UI/Card/Card";
+
+import classes from "./UserDetailPage.module.css";
 
 const UserDetailPage = () => {
   const params = useParams();
   const location = useLocation();
-  const { userName } = location.state;
+  const history = useHistory();
 
-  console.log("params", params);
-  console.log("state", location.state);
+  const { userName } = location.state;
+  const [infoStyle, setInfoStyle] = useState(true);
+  const [logStyle, setLogStyle] = useState(false);
+
+  // console.log("params", params);
+  // console.log("state", location.state);
+
+  const infoClickHandler = () => {
+    setInfoStyle(true);
+    setLogStyle(false);
+  };
+
+  const logClickHandler = () => {
+    setInfoStyle(false);
+    setLogStyle(true);
+  };
+
+  const goBackHandler = () => history.goBack();
+
+  const infoActiveStyle = () => (infoStyle ? classes.active : "");
+  const logActiveStyle = () => (logStyle ? classes.active : "");
 
   return (
     <section className="page">
       <span className="title">{`${userName} (${params.employeeNum})`}</span>
+      <span className="btn" onClick={goBackHandler}>
+        목록으로
+      </span>
+      <article className={classes.tabs}>
+        <span
+          className={`${classes.tab} ${infoActiveStyle()}`}
+          onClick={infoClickHandler}
+        >
+          사용자 정보
+        </span>
+        <span
+          className={`${classes.tab} ${logActiveStyle()}`}
+          onClick={logClickHandler}
+        >
+          사용자 로그
+        </span>
+      </article>
+      <Card type={"nofit"}>
+        {infoStyle && <UserInfo />}
+        {logStyle && <UserLog />}
+      </Card>
     </section>
   );
 };
