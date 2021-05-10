827beb1b8943d1d745b392c209d774d032fca16c
JH_Joo <wjdgnsx94@naver.com>
2021-05-10 22:18:59 +0900

    feat: S04P31A301-208 자동 로그인

--- exec/app/CORC_B/navigations/Navigation.js
+++ exec/app/CORC_B/navigations/Navigation.js
@@ -3,14 +3,17 @@ import { useSelector } from 'react-redux';
 
 import LoginNavigation from './LoginNavigation'
 import TabNavigation from './TabNavigation'
+import StartupScreen from '../screens/StartupScreen';
 
 const Navigation = props => {
   const isAuth = useSelector(state => !!state.auth.token);
+  const didTryAutoLogin = useSelector(state => state.auth.didTryAutoLogin);
 
   return (
     <>
-      {!isAuth && <LoginNavigation />}
       {isAuth && <TabNavigation />}
+      {!isAuth && didTryAutoLogin && <LoginNavigation />}
+      {!isAuth && !didTryAutoLogin && <StartupScreen />}
     </>
   );
 }
