f6b004dad88fb200d2069449e12b6a91e53d8ba3
곽지원 <gjw0407@gmail.com>
2021-05-11 15:47:37 +0900

    feat: S04P31A301-211 가맹점 목록 페이지

---
+++ exec/frontend/web/expense-manager/src/components/Store/StoreInfo.js
@@ -0,0 +1,14 @@
+import { Fragment } from 'react';
+
+import StoreItem from './StoreItem';
+import classes from './StoreList.module.css';
+
+const StoreInfo = (props) => {
+  return (
+    <Fragment>
+      <h1>Store Info</h1>
+    </Fragment>
+  );
+};
+
+export default StoreInfo;
--- exec/frontend/web/expense-manager/src/pages/Store/StoreListPage.js
+++ exec/frontend/web/expense-manager/src/pages/Store/StoreListPage.js
@@ -1,9 +1,53 @@
-import React from "react";
+import { useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
-const StoreListPage = () => (
-  <div className="page">
-    <span className="title">가맹점 목록</span>
-  </div>
-);
+import StoreList from '../../components/Store/StoreList';
+import LoadingSpinner from '../../components/UI/LoadingSpinner/LoadingSpinner';
+import useHttp from '../../hooks/use-http';
+import { getAllStores } from '../../lib/api-store';
+
+import classes from './StoreListPage.module.css';
+
+const StoreListPage = () => {
+  const {
+    sendRequest,
+    status,
+    data: loadedStores,
+    error,
+  } = useHttp(getAllStores, true);
+
+  useEffect(() => {
+    sendRequest();
+    console.log(loadedStores);
+  }, [sendRequest]);
+
+  if (status === 'pending') {
+    return (
+      <div className="centered">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (error) {
+    return <p className="centered focused">{error}</p>;
+  }
+
+  if (status === 'completed' && (!loadedStores || loadedStores.length === 0)) {
+    return <span>가맹점이 없습니다.</span>;
+  }
+
+  return (
+    <div className="page">
+      <span className="title">가맹점 목록</span>
+      <section className={classes.section}>
+        <Link className="btn" to="/store/register">
+          가맹점 등록
+        </Link>
+        <StoreList stores={loadedStores} />
+      </section>
+    </div>
+  );
+};
 
 export default StoreListPage;
