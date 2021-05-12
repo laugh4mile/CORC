85bc844b43beba32f58daa2a2119075aa2876125
JH_Joo <wjdgnsx94@naver.com>
2021-05-12 14:15:30 +0900

    feat: S04P31A301-207 상단 탭 네비게이터

---
+++ exec/app/CORC_B/screens/PaymentHistory/Statistics.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const Statistics = () => {
+    return (
+        <View style={styles.container}>
+            <Text>통계</Text>
+        </View>
+    )
+}
+
+export default Statistics
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+})
+
