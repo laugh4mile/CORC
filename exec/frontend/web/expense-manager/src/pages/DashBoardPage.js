b8065f83de8510fc3f7cb230ccf8d73faf12092f
Hyejin Lee <hichjay@gmail.com>
2021-05-17 03:04:21 +0900

    feat: DashBoard 페이지에 chart 추가
    데이터 가공 필요

--- exec/frontend/web/expense-manager/src/pages/DashBoardPage.js
+++ exec/frontend/web/expense-manager/src/pages/DashBoardPage.js
@@ -1,9 +1,42 @@
 import React from "react";
 
-const DashBoardPage = () => (
-  <div className="page">
-    <span className="title">대쉬보드</span>
-  </div>
-);
+import ActiveShapePieChart from "../components/Chart/ActiveShapePieChart";
+
+import classes from "./DashBoardPage.module.css";
+
+const DashBoardPage = () => {
+  const data_consumedCategory = [
+    { name: "식음료", value: 100000000 },
+    { name: "접대비", value: 12000000 },
+    { name: "오피스 용품", value: 6000000 },
+    { name: "출장비", value: 30000000 },
+  ];
+
+  const data_consumedDepartment = [
+    { name: "디지털 전략부", value: 100000000 },
+    { name: "디지털 사업부", value: 12000000 },
+    { name: "디지털R&D센터", value: 6000000 },
+    { name: "빅데이터센터", value: 30000000 },
+    { name: "업무혁신부", value: 30000000 },
+  ];
+  return (
+    <div className="page">
+      <span className="title">대쉬보드</span>
+      <div className={classes.container}>
+        <section className={classes["section-center"]}></section>
+        <section className={classes["section-right"]}>
+          <article className={classes.card}>
+            <span className={classes.title}>소비 품목 현황</span>
+            <ActiveShapePieChart data={data_consumedCategory} />
+          </article>
+          <article className={classes.card}>
+            <span className={classes.title}>부서별 소비 현황</span>
+            <ActiveShapePieChart data={data_consumedDepartment} />
+          </article>
+        </section>
+      </div>
+    </div>
+  );
+};
 
 export default DashBoardPage;
